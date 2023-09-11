8449639872697506265318af15051c6037f6f11c
Leonid Derevianko <leo.mrakobes@gmail.com>
2023-09-11 23:21:38 +0200

    initial skleton commit

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace adamcameron\symfonythefasttrack;
+namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
--- tests/nodejs/integration/NodeTest.js
+++
@@ -1,8 +0,0 @@
-let chai = require("chai");
-let expect = chai.expect;
-
-describe("Test Node.js", () => {
-    it("has the expected version",  () => {
-        expect(process.version).to.be.match(/v19\.\d+\.\d+/);
-    })
-});
