344ebbd1a1ad546842d76a3ddfa7ac40d0c6f24e
Leonid Derevianko <leo.mrakobes@gmail.com>
2023-09-21 01:50:41 +0200

    Created Message and it's handler to process queue

---
+++ src/Message/CommentMessage.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Message;
+
+readonly class CommentMessage
+{
+    public function __construct(
+        private int   $id,
+        private array $context = []
+    ) {
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getContext(): array
+    {
+        return $this->context;
+    }
+
+
+}
